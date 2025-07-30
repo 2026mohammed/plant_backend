ad4e0395493747a79aeafd36b9b7349cbba00c6c
Mohammed Shaher <shaherm2025@gmail.com>
2025-07-30 23:22:33 +0300

    alter requirements.txt and main.py

--- main.py
+++ main.py
@@ -33,3 +33,6 @@ async def predict(file: UploadFile = File(...)):
     os.remove(file_location)
 
     return result
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Render يعطي متغير PORT تلقائياً
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
